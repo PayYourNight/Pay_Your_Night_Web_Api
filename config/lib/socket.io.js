4ee1d4e84235df074d74e3496b4410ddfea02800
eduardo <eduardo.x.aguiar@gmail.com>
2018-07-06 22:33:36 -0300

    socket

--- config/lib/socket.io.js
+++ config/lib/socket.io.js
@@ -8,7 +8,7 @@ var config = require('../config'),
   https = require('https'),
   cookieParser = require('cookie-parser'),
   passport = require('passport'),
-  socketio = require('socket.io'),
+  socketio = require('socket.io')(server, { origins: 'intense-journey-43070.herokuapp.com:* https://intense-journey-43070.herokuapp.com:*' }),
   session = require('express-session'),
   MongoStore = require('connect-mongo')(session),
   users = {};
@@ -66,6 +66,7 @@ module.exports = function (app, db) {
   } else {
     // Create a new HTTP server
     server = http.createServer(app);
+    console.log('passou no socket');
   }
   // Create a new Socket.io server
   var io = socketio.listen(server);
@@ -77,35 +78,35 @@ module.exports = function (app, db) {
   });
 
   // Intercept Socket.io's handshake request
-  io.use(function (socket, next) {
-    // Use the 'cookie-parser' module to parse the request cookies
-    cookieParser(config.sessionSecret)(socket.request, {}, function (err) {
-      // Get the session id from the request cookies
-      var sessionId = socket.request.signedCookies ? socket.request.signedCookies[config.sessionKey] : undefined;
+  //io.use(function (socket, next) {
+  //  // Use the 'cookie-parser' module to parse the request cookies
+  //  cookieParser(config.sessionSecret)(socket.request, {}, function (err) {
+  //    // Get the session id from the request cookies
+  //    var sessionId = socket.request.signedCookies ? socket.request.signedCookies[config.sessionKey] : undefined;
 
-      if (!sessionId) return next(new Error('sessionId was not found in socket.request'), false);
+  //    if (!sessionId) return next(new Error('sessionId was not found in socket.request'), false);
 
-      // Use the mongoStorage instance to get the Express session information
-      mongoStore.get(sessionId, function (err, session) {
-        if (err) return next(err, false);
-        if (!session) return next(new Error('session was not found for ' + sessionId), false);
+  //    // Use the mongoStorage instance to get the Express session information
+  //    mongoStore.get(sessionId, function (err, session) {
+  //      if (err) return next(err, false);
+  //      if (!session) return next(new Error('session was not found for ' + sessionId), false);
 
-        // Set the Socket.io session information
-        socket.request.session = session;
+  //      // Set the Socket.io session information
+  //      socket.request.session = session;
 
-        //Use Passport to populate the user details
-        passport.initialize()(socket.request, {}, function () {
-          passport.session()(socket.request, {}, function () {
-            if (socket.request.user) {
-              next(null, true);
-            } else {
-              next(new Error('User is not authenticated'), false);
-            }
-          });
-        });
-      });
-    });
-  });
+  //      //Use Passport to populate the user details
+  //      passport.initialize()(socket.request, {}, function () {
+  //        passport.session()(socket.request, {}, function () {
+  //          if (socket.request.user) {
+  //            next(null, true);
+  //          } else {
+  //            next(new Error('User is not authenticated'), false);
+  //          }
+  //        });
+  //      });
+  //    });
+  //  });
+  //});
 
   // Add an event listener to the 'connection' event
   io.on('connection', function (socket) {    
